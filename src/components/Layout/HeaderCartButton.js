8a17ab8d2f1657833170a0cbe99869f22fe2767c
Samyeak <10978827+Samyeak@users.noreply.github.com>
2022-06-11 20:15:25 +0545

    add cart context and reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,20 @@
 import classes from './HeaderCartButton.module.css';
 
-import React from 'react'
+import React, { useContext } from 'react'
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = ({onClick}) => {
+  const cartContext = useContext(CartContext);
+
+  const cartItemCount = cartContext.items.reduce((sum, item)=> sum+ item.amount, 0);
   return (
     <button className={classes.button} onClick={onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{cartItemCount}</span>
     </button>
   )
 }
